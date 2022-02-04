73a0c715c1644f694a4da0b6af973f59cb207e9b
Banica Bogdan <bogdan.banica95@yahoo.com>
2022-02-04 21:44:13 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Index from'./index.css';
+import App from './App';
+import CardList from './CardList';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots';
+
+ReactDOM.render(<App />  ,document.getElementById('root')
+);
+
+reportWebVitals();
---
+++ src/robots.js
@@ -0,0 +1,52 @@
+export const robots = [
+
+{
+	id : 1,
+	name: 'Avatar',
+	username: 'World',
+	email: 'robotworld@yahoo.com'
+},
+
+{
+	id : 2,
+	name: 'Skrillex ',
+	username: 'Ultra',
+	email: 'robotworld@yahoo.com'
+},
+
+{
+	id : 3,
+	name: 'Gandi',
+	username: 'Wild',
+	email: 'robotworld@yahoo.com'
+},
+
+{
+	id : 4,
+	name: 'Death-Star',
+	username: 'STARS',
+	email: 'robotworld@yahoo.com'
+},
+
+{
+	id : 5,
+	name: 'X-treme',
+	username: 'PROTON',
+	email: 'robotworld@yahoo.com'
+},
+
+{
+	id : 6,
+	name: 'Sub-Zero',
+	username: 'Ice',
+	email: 'robotworld@yahoo.com'
+},
+
+{
+	id : 7,
+	name: 'Shooty',
+	username: 'ShotMe',
+	email: 'robotworld@yahoo.com'
+}
+
+];
